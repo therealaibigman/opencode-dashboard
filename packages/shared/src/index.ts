b36352a3d8eb0c5c022d9d2ba1b155ff5efe1bc4
exe.dev user <exedev@tango-whiskey.exe.xyz>
2026-03-04 13:45:35 +0000

    shared: fix exports to use TS module paths

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,6 +1,6 @@
-export * from './events.js';
-export * from './ids.js';
+export * from './events';
+export * from './ids';
 
 // Server-side helpers (do not import from client components)
-export * from './policy.js';
-export * from './patch.js';
+export * from './policy';
+export * from './patch';
